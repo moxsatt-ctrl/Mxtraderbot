4ab5a5701f973c99d42e8d33c984a48240a80199
moxsatt-ctrl <moxsatt@gmail.com>
2025-12-25 21:03:27 +0000

    Refactor bot.py for improved structure and clarity

--- bot.py
+++ bot.py
@@ -491,8 +491,7 @@ def main():
     app.add_error_handler(error_handler)
     app.run_polling(allowed_updates=Update.ALL_TYPES)
 
-if __name__ == "__main__":
-    main()
+
 # ======================
 # MINI SERVIDOR HTTP (Render Free)
 # ======================
@@ -511,3 +510,6 @@ def run_web():
     app.run(host="0.0.0.0", port=port)
 
 threading.Thread(target=run_web, daemon=True).start()
+
+if __name__ == "__main__":
+    main()
